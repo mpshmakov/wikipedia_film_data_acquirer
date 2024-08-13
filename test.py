eb8aaf65ce0f74cadbeab7f25c97172e46f18192
thinkpad_ubuntu <zeras4z4@gmail.com>
2024-08-13 22:28:14 +0100

    created function to get html contents of an url, created parsing logic and a dataframe to store parsed information

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print("hi world")   
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+# print(soup.get_text())
+
+def fetchPage(url):
+    return requests.get(url)
+
+response = fetchPage("https://en.wikipedia.org/wiki/List_of_Academy_Award%E2%80%93winning_films")
+
+soup = bs(response.content, features="html.parser")
+
+trs = soup.find("table", class_="wikitable").find("tbody").find_all("tr")
+
+movies = []
+
+
+for tr in trs:
+    tds = tr.find_all("td")
+    
+
+    film = tr.contents[1].text
+    year = tr.contents[3].text
+    awards = tr.contents[5].text
+    nominations = tr.contents[7].text
+    
+    movies.append([film,year,awards,nominations])
+    
+df = pd.DataFrame(movies, columns=["Film", "Year", "Awards", "Nominations"])
+print(df["Year"].unique())
+
